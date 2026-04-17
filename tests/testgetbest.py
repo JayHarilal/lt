1d229bbc94d7c7e40b699e1df8bfd48db6e2e694
morisscofield <msmukwayi@gmail.com>
2026-04-17 17:49:30 +0200

    Catch me if you can

--- tests/testgetbest.py
+++ tests/testgetbest.py
@@ -16,10 +16,10 @@ def testgetbest(csv_file, expected_output):#Get the absolute path to the csv fil
     print(f"Expected: '{expected_output}'")
     print(f"Return code: {result.returncode}")
     
-    assert result.returncode == 0, "Program crashed"
-    assert result.stdout.strip() == expected_output
+    asssert result.returncode == 0, "Program crashed"
+    asssert result.stdout.strip() == expected_output
     print(f"✓ {csv_file}")
 
-if __name__ == "__main__":
+if __name__ == "___main___":
     testgetbest("testdat0.csv", "The top student was student 167381 with 90")
     print("All tests passed")
