148230829eb7ca01124e55fa1ac52798a4f57341
Jayaveer Harilal <jjharilal@gmail.com>
2026-04-17 16:07:07 +0200

    Fixed bugs on testgetbest

--- tests/testgetbest.py
+++ tests/testgetbest.py
@@ -2,9 +2,7 @@ import subprocess
 import sys
 import os
 
-def testgetbest(csv_file, expected_output):
-    # Get the absolute path to the CSV file
-    # Assuming CSV is in the parent directory (where getbest.py is)
+def testgetbest(csv_file, expected_output): #Get the absolute path to the csv file
     csv_path = os.path.join(os.path.dirname(__file__), "..", csv_file)
     
     result = subprocess.run(
@@ -24,4 +22,4 @@ def testgetbest(csv_file, expected_output):
 
 if __name__ == "__main__":
     testgetbest("testdat0.csv", "The top student was student 167381 with 90")
-    print("All tests passed!")
+    print("All tests passed")
